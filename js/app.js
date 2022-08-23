fdc36193cf9311d4a817a7fae56db2c9c3958f73
Nahid Ibn Ali <nahidibnali7@gmail.com>
2022-08-23 08:19:31 +0600

    added interactive to the site

---
+++ js/app.js
@@ -0,0 +1,51 @@
+// This function is for onclick event handler
+const playersList = [];
+function addToList(event){
+    if(playersList.length > 4){
+        alert('You cannot select players more than five');
+        return;
+    }
+    const button = event;
+    const playerName = event.parentNode.children[0].innerText;
+    const olBody = document.getElementById('ol-body');
+    const li = document.createElement('li');
+    li.innerText = playerName;
+    olBody.appendChild(li);
+    playersList.push(li);
+    button.setAttribute('disabled', true);
+}
+
+// Calculate button event handler
+document.getElementById('btn-calc').addEventListener('click', function(){
+    const perPlayerCostField = document.getElementById('per-player-cost-field')
+    const perPlayerCost = getElementValue('per-player-cost-field', 'value');
+    const numberOfPlayers = playersList.length;
+    const playerExpenses = perPlayerCost * numberOfPlayers;
+    // Clear input value
+    perPlayerCostField.value = '';
+    // Validation
+    if(isNaN(playerExpenses)){
+        alert('Please provide a number');
+        return
+    }
+    setElementValue('player-expenses-field', playerExpenses);
+})
+
+// Calculate total button event handler
+document.getElementById('btn-calc-total').addEventListener('click', function(){
+    const managerCostField = document.getElementById('manager-cost-field')
+    const coachCostField = document.getElementById('coach-cost-field')
+    const playerExpenses = getElementValue('player-expenses-field', false);
+    const managerCost = getElementValue('manager-cost-field', true);
+    const coachCost = getElementValue('coach-cost-field', true);
+    const totalCost = playerExpenses + managerCost + coachCost;
+    // Clear input value
+    managerCostField.value = '';
+    coachCostField.value = '';
+    // Validation
+    if(isNaN(totalCost)){
+        alert('Please provide a number');
+        return
+    }
+    setElementValue('total-cost-field', totalCost);
+})
---
+++ js/utilities.js
@@ -0,0 +1,18 @@
+// This function is for getting element value
+function getElementValue(elementId, isInput){
+    const elementString = document.getElementById(elementId);
+    if(isInput){
+        const elementValue = parseInt(elementString.value);
+        return elementValue;
+    }
+    else{
+        const elementValue = parseInt(elementString.innerText);
+        return elementValue;    
+    }
+}
+
+// This function is for setting element value
+function setElementValue(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
